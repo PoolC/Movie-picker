1bdd2f40fdd9e6a62b6977f4725a264e77d55393
kiuk <ugiya0628@icloud.com>
2021-07-11 13:27:10 +0900

    Header 추가

---
+++ pages/main.styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    width: 100vw;
+    height: 100vh;
+`
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: white;
+    width: 100vw;
+    height: 4rem;
+`
+
+export const Logo = styled.div`
+    display: flex;
+    color: red;
+    font-size: 2rem;
+`
+
+export const NavBar = styled.nav`
+    display: flex;
+    background-color: orange;
+    font-size: 2rem;
+`
+
+export const ContentsBtn = styled.button`
+
+`
+
+export const MyMovieBtn = styled.button`
+
+`
+
+export const LogOutBtn = styled.button`
+
+`
