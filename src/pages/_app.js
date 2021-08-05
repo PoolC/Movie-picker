55329feb719ec7fbe74998a535103d7dc4ea53df
kiuk <ugiya0628@icloud.com>
2021-08-05 14:32:58 +0900

    MockServiceWorker 설정 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,10 @@
 import '../../styles/globals.css'
 import { RecoilRoot } from 'recoil'
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  require('../mocks')
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
